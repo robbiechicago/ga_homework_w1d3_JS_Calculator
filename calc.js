d13ea532203a09ca8009c5a727a34dec9f33f8d5
Rob Holloway <robbiechicago@gmail.com>
2015-08-12 17:43:29 +0100

    created good variables converting string numbers to number numbers, and started working on my if statement.  Included code to capture non-numbers and incorrect operators.

--- calc.js
+++ calc.js
@@ -14,5 +14,33 @@
 var firstNum = prompt("What is your first number?");
 console.log(firstNum);
 var op = prompt("What is your calculation? a = add, s = subtract, m = multiply, d = divide, q = square root, w = power");
+console.log(op);
 var secondNum = prompt("What is your second number?");
+console.log(secondNum);
+
+goodFirstNum = parseFloat(firstNum);
+goodSecondNum = parseFloat(secondNum);
+
+if (typeof(goodFirstNum) != "number" || typeof(goodFirstNum) != "number") {
+  alert("One of your numbers was not actually a number.  Try again, yeah?");
+} else if (op === 'a') {
+  var result = goodFirstNum + goodSecondNum;
+} else if (op === 's') {
+  var result = goodFirstNum - goodSecondNum;
+} else if (op === 'm') {
+  var result = goodFirstNum * goodSecondNum;
+} else if (op === 'd') {
+  var result = goodFirstNum / goodSecondNum;
+// } else if (op === 'q') {
+//   var result = goodFirstNum + goodSecondNum;
+// } else if (op === 'w') {
+//   var result = goodFirstNum + goodSecondNum;
+
+} else {
+  alert("You asked for an operator that I did not offer you.  WTF?")
+}
+
+
+
+
 
