cff976fb6709873919526420ef98308e9538b82d
Rob Holloway <robbiechicago@gmail.com>
2015-08-12 17:22:19 +0100

    index.html is as outlined in class, calc.js I have added three prompts for first and second number, and calculation operator.  Each of these prompts creates a variable

---
+++ calc.js
@@ -0,0 +1,18 @@
+// var op = prompt('Choose an operation');
+// console.log('op', op);
+// var num = prompt('Choose a number');
+// console.log('num', num);
+
+// alert("why is your console not open?")
+
+// var goodNum = parseFloat(num)
+
+//whaaa??
+//parseFloat or parseInt to convert to number with integers.
+//
+
+var firstNum = prompt("What is your first number?");
+console.log(firstNum);
+var op = prompt("What is your calculation? a = add, s = subtract, m = multiply, d = divide, q = square root, w = power");
+var secondNum = prompt("What is your second number?");
+
